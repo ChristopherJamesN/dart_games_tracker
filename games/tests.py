7c82f17c437a00710892ad15177d1b6e077a9af9
ChristopherJamesN <nady.christopher@gmail.com>
2020-01-26 15:59:05 -0600

    Add tests for Game index view.

--- games/tests.py
+++ games/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Game
 
@@ -34,3 +35,31 @@ class GameModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_game = Game(game_date=time)
         self.assertIs(recent_game.was_played_recently(), True)
+
+
+def create_game(score, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Game.objects.create(max_score=score, game_date=time)
+
+
+class GameIndexViewTests(TestCase):
+    def test_no_gamess(self):
+        """
+        If no games exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('games:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No games are available.")
+        self.assertQuerysetEqual(response.context['latest_game_list'], [])
+
+    def test_past_game(self):
+        """
+        games with a game_date in the past are displayed on the
+        index page.
+        """
+        create_game(score=400, days=-30)
+        response = self.client.get(reverse('games:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_game_list'],
+            ['<Game: 400>']
+        )
