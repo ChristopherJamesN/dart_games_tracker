ff45821206ac1d2f81613cc336d0373acbfe779a
ChristopherJamesN <nady.christopher@gmail.com>
2020-01-26 12:04:14 -0600

    Add __str__ methods to models and was_played_recently to Game.

--- games/models.py
+++ games/models.py
@@ -1,12 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Game(models.Model):
     max_score = models.IntegerField(default=300)
     game_date = models.DateTimeField('date game started')
 
+    def __str__(self):
+        return str(self.max_score)
+
+    def was_played_recently(self):
+        return self.game_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Player(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
