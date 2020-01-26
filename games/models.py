72da682e7891edfe813bf7f97d3f9a9747e5b65d
ChristopherJamesN <nady.christopher@gmail.com>
2020-01-26 11:51:04 -0600

    Add Game and Player model.

--- games/models.py
+++ games/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    max_score = models.IntegerField(default=300)
+    game_date = models.DateTimeField('date game started')
+
+
+class Player(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
