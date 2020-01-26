ba3ae8562cb5de888e30e176b1c5eaf7d18b407e
ChristopherJamesN <nady.christopher@gmail.com>
2020-01-26 14:14:22 -0600

    Add detail, results, and scores views.

--- games/urls.py
+++ games/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:game_id>/', views.detail, name='detail'),
+    path('<int:game_id>/results/', views.results, name='results'),
+    path('<int:game_id>/score/', views.score, name='score')
 ]
--- games/views.py
+++ games/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the games index.")
+
+
+def detail(request, game_id):
+    return HttpResponse("You're looking at game %s." % game_id)
+
+
+def results(request, game_id):
+    response = "You're looking at the results of game %s."
+    return HttpResponse(response % game_id)
+
+
+def score(request, game_id):
+    return HttpResponse("You're updating scores on game %s." % game_id)
