2a386b5dfe1d565f3dd9705c1667cd9ce66f105a
ChristopherJamesN <nady.christopher@gmail.com>
2020-01-26 19:33:01 -0600

    Update formatting of admin page.

--- games/admin.py
+++ games/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from .models import Game
+from .models import Game, Player
 
-admin.site.register(Game)
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 4
+
+
+class GameAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['max_score']}),
+        ('Date information', {'fields': ['game_date']}),
+    ]
+    inlines = [PlayerInline]
+    list_display = ('max_score', 'game_date')
+
+
+admin.site.register(Game, GameAdmin)
